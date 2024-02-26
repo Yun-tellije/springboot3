d137e0f30e4ebaf6c74b472da2cb442597211fe9
Yun-tellije <dbswjddms517@gmail.com>
2024-02-26 18:40:40 +0900

    feat: BlogApiController - findById 메소드 추가

--- src/main/java/me/jungeun/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/jungeun/springbootdeveloper/controller/BlogApiController.java
@@ -8,10 +8,7 @@ import me.jungeun.springbootdeveloper.dto.ArticleResponse;
 import me.jungeun.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +36,13 @@ public class BlogApiController {
                 .toList();
 
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("api/articles/{id}") // URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable Long id){ // @PathVariable -> URL에서 id에 해당하는 값 받음
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
