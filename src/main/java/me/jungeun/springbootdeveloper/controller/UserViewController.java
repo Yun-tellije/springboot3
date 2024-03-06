6f982b406bdfd76d40f209e6764ae0c2bbfac330
Yun-tellije <dbswjddms517@gmail.com>
2024-03-06 21:06:52 +0900

    feat: UserViewController 생성 - 뷰 파일 연결 컨트롤러

---
+++ src/main/java/me/jungeun/springbootdeveloper/controller/UserViewController.java
@@ -0,0 +1,13 @@
+package me.jungeun.springbootdeveloper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
