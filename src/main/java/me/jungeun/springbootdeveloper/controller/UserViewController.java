f53373d5ab9ea3802c6e0be465e72950c732a3c1
Yun-tellije <dbswjddms517@gmail.com>
2024-03-06 21:09:41 +0900

    add: UserViewController - signup 반환 컨트롤러

--- src/main/java/me/jungeun/springbootdeveloper/controller/UserViewController.java
+++ src/main/java/me/jungeun/springbootdeveloper/controller/UserViewController.java
@@ -10,4 +10,9 @@ public class UserViewController {
     public String login() {
         return "login";
     }
+
+    @GetMapping("signup")
+    public String signup() {
+        return "signup";
+    }
 }
