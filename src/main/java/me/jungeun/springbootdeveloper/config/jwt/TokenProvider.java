d6982019e121e66c3628a011890283d88ca83174
Yun-tellije <dbswjddms517@gmail.com>
2024-03-14 20:23:15 +0900

    add: TokenProvider - 토큰 기반으로 인증 정보 가져오기

--- src/main/java/me/jungeun/springbootdeveloper/config/jwt/TokenProvider.java
+++ src/main/java/me/jungeun/springbootdeveloper/config/jwt/TokenProvider.java
@@ -1,14 +1,20 @@
 package me.jungeun.springbootdeveloper.config.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import me.jungeun.springbootdeveloper.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -49,5 +55,21 @@ public class TokenProvider {
         } catch(Exception e) { // 복호화 과정에서 에러가 나면 유효하지 않은 토큰
             return false;
         }
+    }
+
+    // 토큰 기반으로 인증 정볼르 가져오는 메서드
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(
+                claims.getSubject(), "", authorities), token, authorities);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser() // 클레임 조회
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
