9637cf3356a0b06f825601e42bebb4ff99caa723
Yun-tellije <dbswjddms517@gmail.com>
2024-03-14 20:10:03 +0900

    feat: TokenProvider 생성 - JWT 토큰 생성

---
+++ src/main/java/me/jungeun/springbootdeveloper/config/jwt/TokenProvider.java
@@ -0,0 +1,41 @@
+package me.jungeun.springbootdeveloper.config.jwt;
+
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import me.jungeun.springbootdeveloper.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
+    }
+
+    // JWT 토큰 생성 메서드
+    private String makeToken(Date expiry, User user) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 typ: JWT
+                // 내용 iss: ajufresh@gmail.com (properties 파일에서 설정한 값0
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(now)       // 내용 iat: 현재 시간
+                .setExpiration(expiry)  // 내용 exp: expiry 멤버 변수값
+                .setSubject(user.getEmail())    // 내용 sub: 유저의 이메일
+                .claim("id", user.getId())  // 클레임 id: 유저 ID
+                // 서명: 비밀값과 함께 해시값을 HS256 방식으로 암호화
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+
+    }
+}
