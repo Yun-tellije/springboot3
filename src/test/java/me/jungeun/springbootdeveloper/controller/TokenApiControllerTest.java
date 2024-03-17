27fad8aa9b785faf4e8ae064015a62f4a75975aa
Yun-tellije <dbswjddms517@naver.com>
2024-03-17 16:16:26 +0900

    rename

--- src/test/java/me/jungeun/springbootdeveloper/controller/TokenApiControllerTest.java
+++ src/test/java/me/jungeun/springbootdeveloper/controller/TokenApiControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -29,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class TokenApiController {
+public class TokenApiControllerTest {
 
     @Autowired
     protected MockMvc mockMvc;
