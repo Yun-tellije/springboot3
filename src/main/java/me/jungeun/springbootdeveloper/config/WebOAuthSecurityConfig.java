97c665c0d6197eb9fa951566eeb9f1b44a65e666
Yun-tellije <dbswjddms517@naver.com>
2024-03-18 17:19:25 +0900

    feat: WebOAuthSecurityConfig - 스프링 시큐리티 기능 비활성화 메서드

---
+++ src/main/java/me/jungeun/springbootdeveloper/config/WebOAuthSecurityConfig.java
@@ -0,0 +1,32 @@
+package me.jungeun.springbootdeveloper.config;
+
+import lombok.RequiredArgsConstructor;
+import me.jungeun.springbootdeveloper.config.jwt.TokenProvider;
+import me.jungeun.springbootdeveloper.config.oauth.OAuth2UserCustomService;
+import me.jungeun.springbootdeveloper.repository.RefreshTokenRepository;
+import me.jungeun.springbootdeveloper.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebOAuthSecurityConfig {
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
+
+    @Bean
+    public WebSecurityCustomizer configure() { // 스프링 시큐리티 기능 비활성화
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers("/img/**", "/css/**", "/js/**");
+    }
+}
