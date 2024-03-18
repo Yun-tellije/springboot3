b9222ec61c7977ef90eff9395bc8464f6ea983b2
Yun-tellije <dbswjddms517@naver.com>
2024-03-18 17:34:22 +0900

    feat: WebOAuthSecurityConfig - filterChain 및 관련 메서드 구현

--- src/main/java/me/jungeun/springbootdeveloper/config/WebOAuthSecurityConfig.java
+++ src/main/java/me/jungeun/springbootdeveloper/config/WebOAuthSecurityConfig.java
@@ -7,11 +7,15 @@ import me.jungeun.springbootdeveloper.repository.RefreshTokenRepository;
 import me.jungeun.springbootdeveloper.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
@@ -28,5 +32,66 @@ public class WebOAuthSecurityConfig {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
                 .requestMatchers("/img/**", "/css/**", "/js/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 1) 토큰 방식으로 인증을 하기 때문에 기존에 사용하던 폼로그인, 세선 비활성화
+        http.csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // 2) 헤더를 확인할 커스텀 필터 추가
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        // 3) 토큰 재발급 URL은 인증 없이 접근 가능하도록 설정, 나머지 API URL은 인증 필요
+        http.authorizeRequests()
+                .requestMatchers("/api/token").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll();
+
+        http.oauth2Login()
+                .loginPage("/login")
+                .authorizationEndpoint()
+                // 4) Authorization 요청과 관련된 상태 저장
+                //    OAuth2에 필요한 정보를 세션이 아닌 쿠키에 저장해서 쓸 수 있도록 인증 요청과 관련된 상태를 저장할 저장소 설정
+                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
+                .and()
+                .successHandler(oAuth2SuccessHandler()) // 5) 인증 성공 시 실행할 핸들러, oauth2Login() 메서드 이후 체인 메서드 수정
+                .userInfoEndpoint()
+                .userService(oAuth2UserCustomService);
+
+        http.logout().logoutSuccessUrl("/login");
+
+        // 6) /api로 시작하는 url인 경우 401 상태 코드를 반환하도록 예외 처리
+        //    exceptionHandling() -> 예외 처리 설정
+        http.exceptionHandling().defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                , new AntPathRequestMatcher("/api/**"));
+
+        return http.build();
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(tokenProvider, refreshTokenRepository, oAuth2AuthorizationRequestBasedOnCookieRepository(), userService);
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
